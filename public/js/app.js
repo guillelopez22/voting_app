0603fe815e087a4446b9c0fe9b5999248e005d0a
Memo <guillelopez_13@hotmail.com>
2018-03-11 19:38:16 -0600

    states

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,33 @@
 class ProductList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			products: [],
+		};
+		this.handleProductUpVote = this.handleProductUpVote.bind(this);
+	}
+	componentDidMount() {
+		this.setState({ products: Seed.products });
+	}
 	handleProductUpVote(productId) {
-		console.log(productId + ' was upvoted.');
+		const nextProducts = this.state.products.map((product) => {
+			if (product.id === productId) {
+				return Object.assign({}, product, {
+					votes: product.votes + 1,
+				});
+			} else {
+				return product;
+			}
+		});
+		this.setState({
+			products: nextProducts,
+		});
 	}
 	render() {
-		const products = Seed.products.sort((a, b) => (
+		const products = this.state.products.sort((a, b) => (
 			b.votes - a.votes
 		));
-		const productComponents = Seed.products.map((product) => (
+		const productComponents = products.map((product) => (
 			<Product
 			key={'product-' + product.id}
 			id={product.id}
@@ -33,6 +54,7 @@ class Product extends React.Component {
 	constructor(props) {
 		super(props);
 		this.handleUpVote = this.handleUpVote.bind(this);
+
 	}
 	handleUpVote() {
 		this.props.onVote(this.props.id);
