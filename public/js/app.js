25281f5f7c6cba186454809cf3d9d103abe170ef
Memo <guillelopez_13@hotmail.com>
2018-03-11 18:03:42 -0600

    fixes

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,32 @@
-class ProductList extends React.Component {
-  render() {
-    return (
-      <div className='ui unstackable items'>
-        Hello, friend! I am a basic React component.
-      </div>
-    );
-  }
+class Product extends React.Component {
+render() {
+return (
+<div className='item'>
+	<div className='image'>
+		<img src='images/products/image-aqua.png' />
+	</div>
+	<div className='middle aligned content'>
+		Your first React Web Application
+		26
+		<div className='description'>
+			<a>Fort Knight</a>
+			<p>Authentic renaissance actors, delivered in just two weeks.</p>
+		</div>
+		<div className='extra'>
+			<span>Submitted by:</span>
+			<img
+			className='ui avatar image'
+			src='images/avatars/daniel.jpg'
+			/>
+		</div>
+	</div>
+</div>
+);
+}
 }
 
 ReactDOM.render(
-  <ProductList />,
-  document.getElementById('content')
+<Product />,
+document.getElementById('content')
 );
 
