51ac6060f99029db9819ded815d95e481a953427
Memo <guillelopez_13@hotmail.com>
2018-03-11 19:22:20 -0600

    propagating events

--- public/js/app.js
+++ public/js/app.js
@@ -1,63 +1,77 @@
 class ProductList extends React.Component {
-render() {
-const productComponents = Seed.products.map((product) => (
-<Product
-key={'product-' + product.id}
-id={product.id}
-title={product.title}
-description={product.description}
-url={product.url}
-votes={product.votes}
-submitterAvatarUrl={product.submitterAvatarUrl}
-productImageUrl={product.productImageUrl}
-/>
-));
-return (
-<div className='ui unstackable items'>
-	{productComponents}
-</div>
-);
-}
+	handleProductUpVote(productId) {
+		console.log(productId + ' was upvoted.');
+	}
+	render() {
+		const products = Seed.products.sort((a, b) => (
+			b.votes - a.votes
+		));
+		const productComponents = Seed.products.map((product) => (
+			<Product
+			key={'product-' + product.id}
+			id={product.id}
+			title={product.title}
+			description={product.description}
+			url={product.url}
+			votes={product.votes}
+			submitterAvatarUrl={product.submitterAvatarUrl}
+			productImageUrl={product.productImageUrl}
+			onVote={this.handleProductUpVote}
+			/>
+		));
+		return (
+			<div className='ui unstackable items'>
+			{productComponents}
+			</div>
+		);
+	}
 }
 
 
 
 class Product extends React.Component {
-render() {
-return (
-<div className='item'>
-	<div className='image'>
-		<img src={this.props.productImageUrl}/>
-	</div>
-	<div className='middle aligned content'>
-		<div className='header'>
-			<a>
-				<i className='large caret up icon' />
+	constructor(props) {
+		super(props);
+		this.handleUpVote = this.handleUpVote.bind(this);
+	}
+	handleUpVote() {
+		this.props.onVote(this.props.id);
+	}
+	render() {
+		return (
+			<div className='item'>
+			<div className='image'>
+			<img src={this.props.productImageUrl}/>
+			</div>
+			<div className='middle aligned content'>
+			<div className='header'>
+			<a onClick={this.handleUpVote}>
+			<i className='large caret up icon' />
 			</a>
 			{this.props.votes}
-		</div>
-		<div className='description'>
+			</div>
+			<div className='description'>
 			<a hfref={this.props.url}>
-				{this.props.title}
+			{this.props.title}
 			</a>
 			<p>
-				{this.props.description}
+			{this.props.description}
 			</p>
-		</div>
-		<div className='extra'>
+			</div>
+			<div className='extra'>
 			<span>Submitted by:</span>
 			<img
 			className='ui avatar image'
 			src={this.props.submitterAvatarUrl}
 			/>
-		</div>
-	</div>
-</div>
-);
-}
+			</div>
+			</div>
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(
-<ProductList />,
-document.getElementById('content')
+	<ProductList />,
+	document.getElementById('content')
 );
